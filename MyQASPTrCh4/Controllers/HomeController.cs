8c0cccac0f8c257ddfe9360d5b70a013ab51c0cc
Vasyl Voloshyn <voloshyn.v.v@gmail.com>
2018-04-11 13:03:07 +0300

    Add Ch4 examples

--- MyQASPTrCh4/Controllers/HomeController.cs
+++ MyQASPTrCh4/Controllers/HomeController.cs
@@ -31,5 +31,31 @@ namespace MyQASPTrCh4.Controllers
                 Price = 275M, Category = "Watersports" };
             return View("Result", (object)String.Format("Category: {0}", myMproduct.Category));
         }
+
+        public ViewResult CreateCollection()
+        {
+            string[] stringArray = { "apple", "orange", "plum" };
+            List<int> intList = new List<int> { 10, 20, 30, 40 };
+            Dictionary<string, int> myDict = new Dictionary<string, int>
+            { { "apple", 10}, {"orange", 20}, {"plum", 30} };
+            return View("Result", (object)stringArray[1]);
+        }
+
+        public ViewResult UseExtension()
+        {
+            ShoppingCart cart = new ShoppingCart
+            {
+                Products = new List<Product>
+                {
+                    new Product { Name = "Kayak", Price = 275M},
+                    new Product { Name = "LifeJacket", Price = 48.95M},
+                    new Product { Name = "Ball", Price = 19.50M},
+                    new Product { Name = "Flag", Price= 34.95M}
+                }
+            };
+            decimal totalCart = cart.TotalPrice();
+            return View("Result", (object)String.Format("Total: {0:c}", totalCart));
+        }
+
     }
 }
---
+++ MyQASPTrCh4/Models/ProductExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyQASPTrCh4.Models
+{
+    public static class ProductExtensionMethods
+    {
+        public static decimal TotalPrice(this ShoppingCart cartParam)
+        {
+            decimal total = 0;
+            foreach(Product prod in cartParam.Products)
+            {
+                total += prod.Price;
+            }
+            return total;
+        }
+    }
+}
