26714bbb34d340c130c06a3536daf22b1fd7700b
Vasyl Voloshyn <voloshyn.v.v@gmail.com>
2018-04-15 15:37:40 +0300

    Ch6 add unit tset

---
+++ EssentialToolsUnittest/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyQASPTrCh6EssentialTools.Models;
+
+namespace EssentialToolsUnittest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private IDiscountHelper getTestObject()
+        {
+            return new MinimumDiscountHelper();
+        }
+        [TestMethod]
+        public void DiscountAbove100()
+        {
+            IDiscountHelper target = getTestObject();
+            decimal total = 200;
+            var discountedTotal = target.ApplyDiscount(total);
+
+            Assert.AreEqual(total * 0.9M, discountedTotal);
+        }
+    }
+}
