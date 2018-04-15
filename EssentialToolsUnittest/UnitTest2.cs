72306e2c97d84369e7e3e3c4ed99e534c2096626
Vasyl Voloshyn <voloshyn.v.v@gmail.com>
2018-04-15 16:18:04 +0300

    Ch6 Added new Unit Test

---
+++ EssentialToolsUnittest/UnitTest2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyQASPTrCh6EssentialTools.Models;
+
+
+namespace EssentialToolsUnittest
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        private Product[] products = {
+                    new Product { Name = "Kayak", Category ="WaterSports", Price = 275M},
+                    new Product { Name = "LifeJacket", Category ="WaterSports",Price = 48.95M},
+                    new Product { Name = "Ball", Category ="Football", Price = 19.50M},
+                    new Product { Name = "Flag", Category ="Football", Price= 34.95M}
+                    };
+        [TestMethod]
+        public void SumProductsCorrectly()
+        {
+            var discounter = new MinimumDiscountHelper();
+            var target = new LinqValueCalculator(discounter);
+            var goalTotal = products.Sum(e => e.Price);
+
+            var result = target.ValueProducts(products);
+            Assert.AreEqual(goalTotal, result);
+        }
+    }
+}
