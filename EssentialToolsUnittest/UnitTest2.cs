38eae8a581bbaba28f609d35f1fb1da22f328003
Vasyl Voloshyn <voloshyn.v.v@gmail.com>
2018-04-15 16:40:46 +0300

    Ch6 fixed moq test

--- EssentialToolsUnittest/UnitTest2.cs
+++ EssentialToolsUnittest/UnitTest2.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MyQASPTrCh6EssentialTools.Models;
+using Moq;
 
 
 namespace EssentialToolsUnittest
@@ -18,12 +19,12 @@ namespace EssentialToolsUnittest
         [TestMethod]
         public void SumProductsCorrectly()
         {
-            var discounter = new MinimumDiscountHelper();
-            var target = new LinqValueCalculator(discounter);
-            var goalTotal = products.Sum(e => e.Price);
-
+            Mock<IDiscountHelper> mock = new Mock<IDiscountHelper>();
+            mock.Setup(m => m.ApplyDiscount(It.IsAny<decimal>()))
+                            .Returns<decimal>(total => total);
+            var target = new LinqValueCalculator(mock.Object);            
             var result = target.ValueProducts(products);
-            Assert.AreEqual(goalTotal, result);
+            Assert.AreEqual(products.Sum(e=>e.Price), result);
         }
     }
 }
